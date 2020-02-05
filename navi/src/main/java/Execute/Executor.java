ac984cf4cc2958bee47870916945e9ca54741194
avodchyts <Vodchyts.alex@gmail.com>
2020-02-05 18:22:49 +0200

    start project

---
+++ navi/src/main/java/Execute/Executor.java
@@ -0,0 +1,10 @@
+package Execute;
+
+import Menu.Menu;
+
+public class Executor {
+    public static void main(String[] args) throws Exception {
+       Menu.menuSelect();
+           }
+}
+
---
+++ navi/src/main/java/Menu/Menu.java
@@ -0,0 +1,54 @@
+package Menu;
+
+import Utils.ScannerInput;
+import org.apache.log4j.Logger;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+public class Menu {
+    private int typeMenu;
+
+    private static final Logger LOGGER = Logger.getLogger(Menu.class);
+    public static FileInputStream getFileInputStream (File file) throws FileNotFoundException {
+        return new FileInputStream(file);
+    }
+
+    public Menu (){}
+    public Menu(int typeMenu){
+        this.typeMenu = typeMenu;
+    }
+    public void setTypeMenu (int typeMenu){
+        this.typeMenu= typeMenu;
+    }
+
+    public int getTypeMenu() {
+        return typeMenu;
+    }
+
+    public static void menuSelect() throws IOException{
+       FileInputStream file =new FileInputStream("D:/navigator/navi/src/main/resources/Menu.properties");
+        Properties properties =new Properties();
+        properties.load(file);
+
+        ScannerInput scannerInput = new ScannerInput();
+        System.out.println("Enter :");
+
+        for(String key : properties.stringPropertyNames()){
+            System.out.println(properties.get(key));
+        }
+        int typeMeny = scannerInput.intInput();
+        switch (typeMeny){
+            case 0:
+
+            break;
+            case 1:
+             //Route.setRouteProperties();
+            break;
+        }
+
+    }
+}
